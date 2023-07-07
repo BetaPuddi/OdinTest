74422f0c10c5164e8273506115b2bf85f456d11a
Reece <wraith0@hotmail.co.uk>
2023-07-07 22:22:41 +0000

    Jul 7, 2023, 11:22 PM

--- Assets/Scripts/AttackAndDefValues.cs
+++ Assets/Scripts/AttackAndDefValues.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "OdinTest/AttackAndDefValues")]
 public class AttackAndDefValues : ScriptableObject
 {
-
+    public int attackValue;
+    public int defenseValue;
 }
