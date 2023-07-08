61935ba2258e7dc268b1e03aa5974e54427c285b
Reece <wraith0@hotmail.co.uk>
2023-07-08 01:33:35 +0000

    Jul 8, 2023, 2:33 AM

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,7 +6,16 @@ using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    public TextMeshProUGUI text = null;
+    public TextMeshProUGUI maxHealthText = null;
+    public TextMeshProUGUI currentHealthText = null;
+    public TextMeshProUGUI maxManaText = null;
+    public TextMeshProUGUI currentManaText = null;
+    public TextMeshProUGUI maxStaminaText = null;
+    public TextMeshProUGUI currentStaminaText = null;
+    public TextMeshProUGUI maxEnergyText = null;
+    public TextMeshProUGUI currentEnergyText = null;
+    public TextMeshProUGUI maxMagicText = null;
+    public TextMeshProUGUI currentMagicText = null;
     public Player player = null;
 
     // Start is called before the first frame update
@@ -19,6 +28,6 @@ public class MenuManager : MonoBehaviour
     void Update()
     {
 
-        text.text = player.maxHealth.ToString();
+        maxHealthText.text = player.maxHealth.ToString();
     }
 }
