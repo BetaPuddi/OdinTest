fd4960176ac4509bcf74b895d70a6a63250b379c
Reece <wraith0@hotmail.co.uk>
2023-07-08 23:25:43 +0000

    Jul 9, 2023, 12:25 AM

--- Assets/Scripts/AttackAndDefValues.cs
+++ Assets/Scripts/AttackAndDefValues.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using Sirenix.OdinInspector;
 
-[CreateAssetMenu(fileName = "AttackAndDefValues", menuName = "Attributes and Stats/AttackAndDefValues", order = 0)]
+[CreateAssetMenu(fileName = "AttackAndDefValues", menuName = "Attributes and Resources/AttackAndDefValues", order = 0)]
 public class AttackAndDefValues : ScriptableObject
 {
     public int attackValue;
