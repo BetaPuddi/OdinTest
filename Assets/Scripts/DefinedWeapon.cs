962ddb559170ef0bae5d46e1183a9bf1466ff93f
Reece <wraith0@hotmail.co.uk>
2023-07-08 00:11:10 +0000

    Jul 8, 2023, 1:11 AM

--- Assets/Scripts/DefinedWeapon.cs
+++ Assets/Scripts/DefinedWeapon.cs
@@ -16,5 +16,30 @@ public class DefinedWeapon : WeaponBase
         //totalMaxHealth = CalculateWeaponStatsTotals();
     }
 
+    public int CalculateMaterialStats()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialStatValues.maxHealth;
+        }
 
+        return total;
+    }
+
+    public int CalculateWeaponTypeStats()
+    {
+        int total = 0;
+        for (int i = 0; i < weaponType.Length; i++)
+        {
+            total += this.weaponType[i].weaponTypeStatValues.maxHealth;
+        }
+        return total;
+    }
+
+    public int CalculateWeaponStatsTotals()
+    {
+        int total = this.statValues.maxHealth + this.materialsMaxHealth + this.weaponTypeMaxHealth;
+        return total;
+    }
 }
