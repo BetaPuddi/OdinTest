4b0f57431df8264be379b9a7488f9c8202b2c945
Reece <wraith0@hotmail.co.uk>
2023-07-08 22:13:41 +0000

    Jul 8, 2023, 11:13 PM

--- Assets/Scripts/DefinedFeetArmour.cs
+++ Assets/Scripts/DefinedFeetArmour.cs
@@ -53,4 +53,94 @@ public class DefinedFeetArmour : ArmourBase
         }
         return total;
     }
+
+    public int CalculateMaterialStrength()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.strengthAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialDexterity()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.dexterityAtt;
+        }
+        return total;
+    }
+
+    public int ClculateMaterialConstitution()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.constitutionAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialIntelligence()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.intelligenceAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialWisdom()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.wisdomAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialFocus()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.focusAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialCharisma()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.charismaAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialLuck()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.luckAtt;
+        }
+        return total;
+    }
+
+    public int CalculateMaterialMind()
+    {
+        int total = 0;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            total += this.materials[i].materialAttributeValues.mindAtt;
+        }
+        return total;
+    }
 }
