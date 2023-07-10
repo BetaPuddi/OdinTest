e28a6ec69057f43e4066a51a099805bf71fb1cfe
Reece <wraith0@hotmail.co.uk>
2023-07-10 01:36:42 +0100

    inventory list update method

--- Assets/Scripts/UI/PlayerInventoryPanel.cs
+++ Assets/Scripts/UI/PlayerInventoryPanel.cs
@@ -7,23 +7,32 @@ public class PlayerInventoryPanel : MonoBehaviour
     public GameObject inventoryPanel;
     public GameObject itemPrefab;
     public Inventory inventory;
+    public List<GameObject> itemPrefabs = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        foreach (InventoryItem inventoryItem in inventory.inventoryItems)
-        {
-            GameObject newInventoryItem = Instantiate(itemPrefab, inventoryPanel.transform) as GameObject;
-            InventoryItemPrefab newItem = newInventoryItem.GetComponent<InventoryItemPrefab>();
-            newItem.SetItem(inventoryItem);
-        }
+        UpdateInventory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*foreach (InventoryItem inventoryItem in inventory.inventoryItems)
+
+    }
+
+    public void UpdateInventory()
+    {
+        foreach (GameObject prefab in itemPrefabs)
         {
-            Instantiate(itemPrefab, inventoryPanel.transform);
-        }*/
+            Destroy(prefab);
+        }
+        itemPrefabs.Clear();
+        foreach (InventoryItem inventoryItem in inventory.inventoryItems)
+        {
+            GameObject newInventoryItem = Instantiate(itemPrefab, inventoryPanel.transform) as GameObject;
+            InventoryItemPrefab newItem = newInventoryItem.GetComponent<InventoryItemPrefab>();
+            newItem.SetItem(inventoryItem);
+            itemPrefabs.Add(newInventoryItem);
+        }
     }
 }
