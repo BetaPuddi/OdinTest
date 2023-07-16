4fcd41e7213bebfcf02e1f66d484a64b1166ce8a
Reece <wraith0@hotmail.co.uk>
2023-07-16 16:02:59 +0100

    minor commit

--- Assets/Scripts/UI/InventoryItemPrefab.cs
+++ Assets/Scripts/UI/InventoryItemPrefab.cs
@@ -17,9 +17,7 @@ public class InventoryItemPrefab : MonoBehaviour
     public int sortingNumber;
     // Start is called before the first frame update
     void Start()
-    {
-
-    }
+    { }
 
     // Update is called once per frame
     void Update()
